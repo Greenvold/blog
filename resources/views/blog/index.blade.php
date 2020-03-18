db10d5352de065bcac751669f8880972ff3c135e
Rudolf <rudolf.bruder@gmail.com>
2020-03-18 17:13:35 +0100

    created general folder structure for blog and posts

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('title')
+Blog - home
+@endsection
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-lg-9">
+            List of posts
+        </div>
+        <div class="col-lg-3">
+            Side bare
+        </div>
+    </div>
+</div>
+@endsection
